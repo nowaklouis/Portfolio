d41c66cd48dca88b12620631cba34a20030e9401
louis nowak <nowak.louis@gmail.com>
2023-02-03 14:00:08 +0100

    up default

--- Front/next.config.js
+++ Front/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    KEY_SENDGRID:
-      "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",
-  },
 };
 
 module.exports = nextConfig;
